d2c12057d518826b441cba04b12d9fdabf13b9c4
Giorgi <giorgizanqaidzeOfficial@gmail.com>
2023-02-10 00:24:19 +0400

    import all neccesary files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Landing} from './pages/Landing'
+import {ResumeFirstPage} from './pages/ResumeFirstPage'
+import {ResumeSecondPage} from './pages/ResumeSecondPage'
+import {ResumeThirdPage} from './pages/ResumeThirdPage'
+import {ResultResume} from './pages/ResultResume'
+import {Error} from './pages/Error'
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div className="app">
+       
     </div>
   );
 }
