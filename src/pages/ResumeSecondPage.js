2f9a4020899be7573aaf33dfc41c4dc996a58b0a
Giorgi <giorgizanqaidzeOfficial@gmail.com>
2023-02-10 09:38:22 +0400

    import all necessary files

---
+++ src/pages/ResumeSecondPage.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import Vector from "../images/Prev-logo.png"
+import { ResumeBuilder } from "../components/ResumeBuilder"
+import { useNavigate} from 'react-router-dom'
+import { useGlobalContext } from '../context'
+import '../css/secondPage.css'
+
+
+
+
+
